44b20d72e0081672428ae1a3e6553e59538ef8ec
skolopuxa <@rbiu.local>
2025-11-21 13:04:35 +0500

    detail2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 import './style.css'
+import 'flowbite';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+ let swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+    });
